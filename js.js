1237d27cc49d771bf404d4b4bea61b0456fe25cb
= <biernat521@gmail.com>
2021-06-02 16:16:07 +0200

    Add new files

---
+++ js.js
@@ -0,0 +1,381 @@
+const countriesInfs = [
+ {
+  country : 'Albania',
+  capitalCity : 'Tirana',
+  president : 'Ilir Meta',
+  currency : 'Lek Albański',
+  imgPath : 'img/albania.png',
+ },
+ {
+  country : 'Andora',
+  capitalCity : 'Andora',
+  president : '-',
+  currency : 'Euro',
+  imgPath : 'img/andora.png',
+ },
+ {
+  country : 'Austria',
+  capitalCity : 'Wiedeń',
+  president : 'Alexander Van der Bellen',
+  currency : 'Euro',
+  imgPath  :'img/austria.png',
+ },
+ {
+  country : 'Belgia',
+  capitalCity : '	Bruksela',
+  president : 'Król Filip I Koburg',
+  currency : 'Euro',
+  imgPath : 'img/belgia.png',
+ },
+ {
+  country : 'Bośnia i Hercegowina',
+  capitalCity : 'Sarajewo',
+  president : 'Milorad Dodik',
+  currency : 'Marka zamienna',
+  imgPath : 'img/bośnia.png',
+ },
+ 
+ {
+  country : 'Bułgaria',
+  capitalCity : 'Sofia',
+  president : 'Rumen Radew',
+  currency : 'Lew bułgarski',
+  imgPath : 'img/bułgaria.png',
+ },
+ {
+  country : 'Chorwacja',
+  capitalCity : 'Zagrzeb',
+  president : 'Zoran Milanović',
+  currency : 'Lew bułgarski',
+  imgPath : 'img/chorwacja.jpg',
+ },
+ {
+  country : 'Czarnogóra',
+  capitalCity : 'Podgorica',
+  president : 'Milo Đukanović',
+  currency : 'Euro',
+  imgPath : 'img/czarnogóra.jpg',
+ },
+ {
+  country : 'Czechy',
+  capitalCity : 'Praga',
+  president : 'Miloš Zeman',
+  currency : 'Korona czeska',
+  imgPath : 'img/czechy.jpg',
+ },
+ {
+  country : 'Dania',
+  capitalCity : 'Kopenhaga',
+  president : 'Królowa Małgorzata II',
+  currency : 'Korona duńska',
+  imgPath : 'img/dania.jpg',
+ },
+ {
+  country : 'Estonia',
+  capitalCity : 'Tallinn',
+  president : 'Kersti Kaljulaid',
+  currency : 'Euro',
+  imgPath : 'img/estonia.jpg',
+ },
+ {
+  country : 'Finlandia',
+  capitalCity : 'Helsinki',
+  president : 'Sauli Niinistö',
+  currency : 'Euro',
+  imgPath : 'img/finlandia.jpg',
+ },
+ {
+  country : 'Francja',
+  capitalCity : 'Paryż',
+  president : 'Emmanuel Macron',
+  currency : 'Euro',
+  imgPath : 'img/francja.jpg',
+ },
+ {
+  country : 'Grecja',
+  capitalCity : 'Ateny',
+  president : 'Ekaterini Sakielaropulu',
+  currency : 'Euro',
+  imgPath : 'img/grecja.jpg',
+ },
+ {
+  country : 'Hiszpania',
+  capitalCity : 'Madryt',
+  president : 'Filip VI Burbon',
+  currency : 'Euro',
+  imgPath : 'img/hiszpania.jpg',
+ },
+ {
+  country : 'Holandia',
+  capitalCity : 'Amsterdam',
+  president : 'Król Wilhelm Aleksander',
+  currency : 'Euro',
+  imgPath : 'img/holandia.jpg',
+ },
+ {
+  country : 'Irlandia',
+  capitalCity : 'Dublin',
+  president : 'Michael D. Higgins',
+  currency : 'Euro',
+  imgPath : 'img/irlandia.jpg',
+ },
+ {
+  country : 'Islandia',
+  capitalCity : 'Reykjavík',
+  president : 'Guðni Th. Jóhannesson',
+  currency : 'Korona islandzka',
+  imgPath : 'img/islandia.jpg',
+ },
+ {
+  country : 'Kazachstan',
+  capitalCity : 'Nur-Sułtan',
+  president : 'Kasym-Żomart Tokajew',
+  currency : 'Tenge',
+  imgPath : 'img/kazachstan.png',
+  }, 
+  {
+   country : 'Liechtenstein',
+   capitalCity : 'Vaduz',
+   president : 'Książę Jan Adam II',
+   currency : 'Frank szwajcarski',
+   imgPath : 'img/liechtenstein.png',
+  }, 
+  {
+   country : 'Litwa',
+   capitalCity : 'Wilno',
+   president : 'książę Jan Adam I',
+   currency : 'Euro',
+   imgPath : 'img/litwa.jpg',
+  }, 
+  {
+   country  :'Luksemburg',
+   capitalCity : 'Luksemburg',
+   president : 'wielki książę Henryk',
+   currency : 'Euro',
+   imgPath : 'img/luksemburg.png',
+  }, 
+  {
+   country  :'Łotwa',
+   capitalCity : 'Ryga',
+   president  :'Egils Levits',
+   currency : 'Euro',
+   imgPath :'img/łotwa.png',
+  }, 
+  {
+   country : 'Macedonia Północna',
+   capitalCity : 'Skopje',
+   president : 'Stewo Pendarowski',
+   currency : 'Denar Macedoński',
+   imgPath : 'img/macedonia.png',
+  }, 
+  {
+   country : 'Malta',
+   capitalCity : 'Valetta',
+   president : 'George William Vella',
+   currency : 'Euro',
+   imgPath : 'img/malta.png',
+  }, 
+  {
+   country : 'Mołdawia',
+   capitalCity : 'Kiszyniów',
+   president : 'Maia Sandu',
+   currency : 'Lej Mołdawii',
+   imgPath : 'img/mołdawia.png',
+  }, 
+  {
+   country : 'Monako',
+   capitalCity : 'Monako',
+   president : 'Książę Albert II Grimaldi',
+   currency : 'Euro',
+   imgPath : 'img/monako.png',
+  },
+  {
+   country : 'Niemcy',
+   capitalCity : 'Berlin',
+   president : 'Angela Merkel',
+   currency : 'Euro',
+   imgPath : 'img/niemcy.png',
+  }, 
+  {
+   country : 'Norwegia',
+   capitalCity : 'Oslo',
+   president : 'król Harald V',
+   currency : 'Korona norweska',
+   imgPath : 'img/norwegia.png',
+  },
+  {
+   country : 'Polska',
+   capitalCity : 'Warszawa',
+   president : 'Andrzej Duda',
+   currency : 'Polski Złoty',
+   imgPath : 'img/polska.png',
+  },
+  {
+   country : 'Portugalia',
+   capitalCity : 'Lizbona',
+   president : 'Marcelo Rebelo de Sousa',
+   currency : 'Euro',
+   imgPath : 'img/portugalia.png',
+  }, 
+  {
+   country : 'Rosja',
+   capitalCity : 'Moskwa',
+   president : 'Władimir Putin',
+   currency : 'Rubel rosyjski',
+   imgPath : 'img/rosja.png',
+  }, 
+  {
+   country : 'Rumunia',
+   capitalCity : 'Bukareszt',
+   president : 'Klaus Iohannis',
+   currency : 'Lej rumuński',
+   imgPath : 'img/rumunia.png',
+  },
+  {
+   country : 'San Marino',
+   capitalCity : 'San Marino',
+   president : 'Gian Carlo Venturini i Marco Nicolini',
+   currency : 'Euro',
+   imgPath : 'img/sanmarino.png',
+  }, 
+  {
+   country : 'Serbia',
+   capitalCity : 'Belgrad',
+   president : 'Aleksandar Vučić',
+   currency : 'Dinar serbski',
+   imgPath : 'img/serbia.png',
+  },
+  {
+   country : 'Słowacja',
+   capitalCity : 'Bratysława',
+   president : 'Zuzana Čaputová',
+   currency : 'Euro',
+   imgPath : 'img/słowacja.png',
+  }, 
+  {
+   country : 'Słowenia',
+   capitalCity : 'Lublana',
+   president : 'Borut Pahor',
+   currency : 'Euro',
+   imgPath : 'img/słowenia.png',
+  }, 
+  {
+   country : 'Szwajcaria',
+   capitalCity : 'Berno ',
+   president : 'Guy Parmelin',
+   currency : 'Frank szwajcarski',
+   imgPath : 'img/szwajcaria.png',
+  }, 
+  {
+   country : 'Szwecja',
+   capitalCity : 'Sztokholm',
+   president : 'król Karol XVI Gustaw',
+   currency : 'Korona szwedzka',
+   imgPath : 'img/szwecja.png',
+  },
+  {
+   country : 'Turcja',
+   capitalCity : 'Ankara',
+   president : 'Recep Tayyip Erdoğan',
+   currency : 'Lira turecka',
+   imgPath : 'img/turcja.png',
+  }, 
+  {
+   country : 'Ukraina',
+   capitalCity : 'Kijów',
+   president : 'Wołodymyr Zełenski',
+   currency : 'Hrywna',
+   imgPath : 'img/ukraina.png',
+  }, 
+  {
+   country : 'Watykan',
+   capitalCity : 'Watykan',
+   president : 'Giuseppe Bertello',
+   currency : 'Euro',
+   imgPath : 'img/watykan.jpg',
+  }, 
+  {
+   country : 'Węgry',
+   capitalCity : 'Budapeszt',
+   president : 'János Áder',
+   currency : 'Forint',
+   imgPath : 'img/węgry.jpg',
+  }, 
+  {
+   country : 'Włochy',
+   capitalCity : 'Rzym',
+   president : 'Sergio Mattarella',
+   currency : 'Euro',
+   imgPath : 'img/włochy.png',
+  }, 
+  {
+   country : 'Anglia',
+   capitalCity : 'Londyn',
+   president : 'królowa Elżbieta II',
+   currency : 'Brytyjski funt szterling',
+   imgPath : 'img/anglia.jpg',
+  }, 
+  {
+   country : 'Kosowo',
+   capitalCity : 'Prisztina',
+   president : 'Vjosa Osmani',
+   currency : 'Euro',
+   imgPath : 'img/kosowo.png',
+  }, 
+  {
+   country : 'Nadniestrze',
+   capitalCity : 'Tyraspol',
+   president : 'Wadim Krasnosielski',
+   currency : 'Rubel naddniestrzański',
+   imgPath : 'img/nadniestrze.png',
+  }, 
+  {
+   country : 'Walia',
+   capitalCity : 'Cardiff',
+   president : '	królowa Elżbieta II',
+   currency : 'Brytyjski funt szterling',
+   imgPath : 'img/walia.png',
+  },
+  {
+    country : 'Białoruś',
+    capitalCity : 'Mińsk',
+    president : 'Alaksandr Łukaszenka',
+    currency : 'Rubel białoruski',
+    imgPath : 'img/białoruś.png',
+   }, 
+]
+
+
+
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const photo = document.querySelector('.jpg-flag');
+const nameInf = document.querySelector('.country');
+const cityInf = document.querySelector('.capitalCity');
+const currencyInf = document.querySelector('.currency');
+const presidentInf = document.querySelector('.president');
+
+
+
+const showCountry = () => {
+  const inputVal = countriesInfs.find(el => el.country === input.value)
+
+  if(inputVal){
+    nameInf.textContent = inputVal.country
+    cityInf.textContent = inputVal.capitalCity
+    currencyInf.textContent = inputVal.currency
+    presidentInf.textContent = inputVal.president
+    photo.setAttribute('src' , inputVal.imgPath)
+  }
+}
+
+const enterCheck = () => {
+  if(event.keyCode === 13){
+   showCountry();
+  }
+ }
+ 
+ showCountry();
+ btn.addEventListener('click' , showCountry)
+ input.addEventListener('keyup', enterCheck)
